9b9745213af438d7c1d8afd46544da026495a27a
Ciensprog <jorg.leonrd@gmail.com>
2023-06-23 23:46:36 -0600

    feat: add new icons

---
+++ src/assets/icons/chat-remove.tsx
@@ -0,0 +1,19 @@
+/*
+|-------------------------------------------------------------------------------
+| Icon
+|-------------------------------------------------------------------------------
+*/
+
+export function IconChatRemove() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.25"
+        d="M7.5 10h5M10 18.333a8.333 8.333 0 1 0 0-16.666 8.333 8.333 0 0 0-7.218 12.5l-.699 3.75 3.75-.699A8.295 8.295 0 0 0 10 18.333Z"
+      />
+    </svg>
+  )
+}
